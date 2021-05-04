d46b35cbe479fba63fae8d8adb38de52b3acb938
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2021-05-04 14:14:15 -0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-ln -s ./dotFiles/.vim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/zshrc ~/.zshrc
-nvim -c 'so %|PlugInstall' ~/.vimrc
+ln -s ~/dotFiles/.vim/init.vim ~/.config/nvim/init.vim
+nvim -c 'so %|PlugInstall' ~/.config/nvim/init.vim
