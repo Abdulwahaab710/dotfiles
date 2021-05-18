efeebab1b9bb52ec4a6d160f9beed94c36cd7a96
Abdulwahaab Ahmed <Abdulwahaab710@users.noreply.github.com>
2021-05-18 12:30:41 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
+mkdir -p $HOME/.config/nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-ln -s ~/dotFiles/.vim/init.vim ~/.config/nvim/init.vim
-nvim -c 'so %|PlugInstall' ~/.config/nvim/init.vim
+ln -fs $HOME/dotfiles/.vim/init.vim $HOME/.config/nvim/init.vim
+nvim -c 'so %|PlugInstall' $HOME/.config/nvim/init.vim
