109d4364ad0816a071faf3abbba7425b1d817da8
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2021-06-14 00:37:50 -0400

    Extract lua plugin configs

---
+++ nvim/lua/plugins/nvim-lsp.lua
@@ -0,0 +1,39 @@
+local nvim_lsp = require('lspconfig')
+
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    vsnip = true;
+    ultisnips = true;
+  };
+}
+
+local servers = {
+  -- 'pyright',
+  -- 'gopls',
+  -- 'rust_analyzer',
+  'tsserver',
+  'solargraph'
+}
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+  }
+end
