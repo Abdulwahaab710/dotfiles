2abc21432cd22aa01023830b6ed69152b0774cb5
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2021-07-12 14:23:56 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
-mkdir -p $HOME/.config/nvim
+mkdir -p "$HOME/.config/nvim"
+
+# Install neovim 0.5
+if [ -f "/usr/bin/apt" ]; then
+  sudo add-apt-repository ppa:neovim-ppa/unstable -y && sudo apt-get update -y && sudo apt-get install neovim -y
+fi
+
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-ln -fs $HOME/dotfiles/.vim/init.vim $HOME/.config/nvim/init.vim
-nvim -c 'so %|PlugInstall' $HOME/.config/nvim/init.vim
+
+ln -fs "$HOME/dotfiles/.vim/init.vim" "$HOME/.config/nvim/init.vim"
+
+install_prezto() {
+  zsh
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+}
+
+ln -fs "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+ln -fs "$HOME/dotfiles/.zprezto" "$HOME/.zprezto"
+
+nvim -c 'so %|PlugInstall' "$HOME/.config/nvim/init.vim"
