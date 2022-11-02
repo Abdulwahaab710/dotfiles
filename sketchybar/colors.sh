489f856e01b263baca238440837e909f6bd3127d
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2022-11-02 14:04:55 -0400

    Add sketchybar

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -11,16 +11,3 @@ ORANGE=0xfff5a97f
 MAGENTA=0xffc6a0f6
 GREY=0xff939ab7
 TRANSPARENT=0x00000000
-
-# General bar colors
-BAR_COLOR=0xcc24273a #$GREY # Grey bar
-ICON_COLOR=$WHITE # Color of all icons
-LABEL_COLOR=$WHITE # Color of all labels
-
-POPUP_BACKGROUND_COLOR=$BLACK
-POPUP_BORDER_COLOR=$GREEN
-
-SHADOW_COLOR=$BLACK
-
-# Item specific special colors
-SPOTIFY_GREEN=0xff1db954
