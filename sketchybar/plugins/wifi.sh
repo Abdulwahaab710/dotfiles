f623853131eb617caae03bfdba7677ffa5b15af8
Abdulwahaab Ahmed <abdulwahaab.ahmed710@gmail.com>
2023-06-01 22:04:30 -0400

    Update sketchybar

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -4,6 +4,7 @@ CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/
 SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
 CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
 
+echo "$SSID"
+
 POPUP_OFF="sketchybar --set wifi.control popup.drawing=off"
 POPUP_CLICK_SCRIPT="sketchybar --set \$NAME popup.drawing=toggle"
-
