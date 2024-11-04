8f62d3f31d3b13a1c1e6a57be8510bf1f934df37
Abdulwahaab Ahmed <abdulwahaab.ahmed@shopify.com>
2024-11-04 18:52:01 -0500

    Set wallpaper for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,9 +9,6 @@ config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font 'PragmataProMonoLiga Nerd Font'
 config.font_size = 15
 
-config.window_background_opacity = 0.7
-config.macos_window_background_blur = 25
-
 config.window_decorations = "RESIZE"
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -23,5 +20,33 @@ config.window_padding = {
   bottom = 0,
 }
 
+local home = os.getenv("HOME")
+
+-- The art is a bit too bright and colorful to be useful as a backdrop
+-- for text, so we're going to dim it down to 10% of its normal brightness
+local dimmer = { brightness = 0.05 }
+
+config.enable_scroll_bar = false
+
+config.background = {
+  -- This is the deepest/back-most layer. It will be rendered first
+  {
+    source = {
+      File = home .. "/src/github.com/abdulwahaab710/dotfiles/wallpapers/grendizer-1.png",
+    },
+    -- The texture tiles vertically but not horizontally.
+    -- When we repeat it, mirror it so that it appears "more seamless".
+    -- An alternative to this is to set `width = "100%"` and have
+    -- it stretch across the display
+    repeat_x = "NoRepeat",
+    hsb = dimmer,
+
+    vertical_align = "Middle",
+    -- When the viewport scrolls, move this layer 10% of the number of
+    -- pixels moved by the main viewport. This makes it appear to be
+    -- further behind the text.
+    attachment = "Fixed",
+  },
+}
 -- and finally, return the configuration to wezterm
 return config
