238fafb5f1633710cc83ba11572d742429cd687d
Abdulwahaab Ahmed <abdulwahaab.ahmed@shopify.com>
2026-04-21 15:27:45 -0400

    feat(nvim): add nvim-devdocs plugin

---
+++ nvim/lua/plugins/devdocs.lua
@@ -0,0 +1,9 @@
+return {
+  "luckasRanarison/nvim-devdocs",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  opts = {}
+}
