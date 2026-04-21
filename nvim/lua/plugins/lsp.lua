5483a649e1375c711cc38cde7d81afa0d4d46d47
Abdulwahaab Ahmed <abdulwahaab.ahmed@shopify.com>
2026-04-21 15:27:23 -0400

    feat(nvim): add semgrep lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -51,7 +51,8 @@ return {
         -- solargraph = {},
         -- sorbet = {},
         lua_ls = {},
-        ast_grep = {}
+        ast_grep = {},
+        semgrep = {},
         -- shellcheck = {},
       }
 
@@ -275,8 +276,16 @@ return {
         -- these are the default options, you only need to specify
         -- options you'd like to change from the default
         cmd = { 'ast-grep', 'lsp' },
-        filetypes = { "c", "cpp", "rust", "go", "java", "python", "javascript", "typescript", "html", "css", "kotlin", "dart", "lua", "ruby" },
+        filetypes = { "c", "cpp", "rust", "go", "java", "python", "javascript", "typescript", "html", "css", "kotlin", "dart", "lua", "ruby", "erb" },
         root_dir = require('lspconfig.util').root_pattern('sgconfig.yaml', 'sgconfig.yml')
+      })
+
+      require('lspconfig').semgrep.setup({
+        -- these are the default options, you only need to specify
+        -- options you'd like to change from the default
+        cmd = { 'semgrep lsp' },
+        -- filetypes = { "c", "cpp", "rust", "go", "java", "python", "javascript", "typescript", "html", "css", "kotlin", "dart", "lua", "ruby", "erb" },
+        -- root_dir = require('lspconfig.util').root_pattern('.semgrep.yml', '.semgrep.yaml', '.git')
       })
 
       require('lspconfig').lua_ls.setup {
