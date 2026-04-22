d5e52f8a357a269c6d1fcad309531fb808461b46
Abdulwahaab Ahmed <abdulwahaab.ahmed@shopify.com>
2026-04-21 22:44:40 -0400

    feat(nvim): migrate nvim-treesitter to main

--- nvim/plugin/10-core.lua
+++ nvim/plugin/10-core.lua
@@ -1,19 +1,8 @@
-vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(ev)
-    if ev.data.spec.name ~= 'nvim-treesitter' then return end
-    if ev.data.kind == 'update' then
-      pcall(vim.cmd, 'TSUpdate')
-    end
-  end,
-})
-
 vim.pack.add({
   'https://github.com/nvim-lua/plenary.nvim',
   'https://github.com/nvim-tree/nvim-web-devicons',
   'https://github.com/windwp/nvim-autopairs',
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'master' },
-  'https://github.com/nvim-treesitter/playground',
-  'https://github.com/nvim-treesitter/nvim-treesitter-refactor',
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' },
   'https://github.com/RRethy/nvim-treesitter-endwise',
 })
 
