c8efbed19804f854708dd4b202ef214c99d31438
Abdulwahaab Ahmed <abdulwahaab.ahmed@shopify.com>
2026-05-26 14:50:21 -0400

    chore(nvim): clean plugin configuration

--- nvim/lua/plugins_config/treesitter.lua
+++ nvim/lua/plugins_config/treesitter.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = {
     "ruby", "rust", "javascript", "html", "css", "json", "sql", "lua", "yaml", "toml", "dockerfile", "bash", "python",
-    "git_config", "gitignore", "vim", "markdown", "latex", "regex", "cmake", "cpp", "c", "tsx", "typescript",
+    "git_config", "gitignore", "vim", "markdown", "regex", "cmake", "cpp", "c", "tsx", "typescript",
     "go"
   },                                                   -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = { "phpdoc", "tree-sitter-phpdoc" }, -- List of parsers to ignore installing
